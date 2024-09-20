44f27824bed93aace4c4cbe6a85d7d949f2aa1dd
Anna Júlia do Prado <147573503+jujobs@users.noreply.github.com>
2024-09-20 15:02:31 -0300

    Delete loginjubs directory

--- loginjubs/javascript.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    let x = document.forms["jequiti"]["name"].value;
-    let y = document.forms["jequiti"]["email"].value;
-    let z = document.forms["jequiti"]["senha"].value;
-    if (x == "" && y == "" && z == "") {
-      alert("BOTE A INFORMAÇÃO !!!");
-      return false;
-    }
-  }
